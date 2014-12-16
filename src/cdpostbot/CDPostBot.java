49bf51b7f9f49ef27bffd37998a2a647f963a795
Philip <Philip@Kids-Dec-2013>
2014-12-16 17:11:59 -0500

    live updating on added events. TODO: Update .help

--- src/cdpostbot/CDPostBot.java
+++ src/cdpostbot/CDPostBot.java
@@ -15,13 +15,15 @@ import java.net.URLDecoder;
 
 public class CDPostBot extends ListenerAdapter
 {
-    
+
     static String eventListString;
     static PircBotX bot = new PircBotX();
     static GoogleResults googleMsgResults;
     static JsonArray eventList;
     static String eventMatchesString;
     static JsonArray eventMatches;
+    static List<String> addedEvents = new ArrayList<String>();
+    static Map<String, Integer> eventLastTimes = new HashMap<String, Integer>();
 
     public CDPostBot()
     {
@@ -36,7 +38,7 @@ public class CDPostBot extends ListenerAdapter
     {
         bot.sendMessage(chan, outputMessage);
     }
-    
+
     public void privmsg(User user, String outputMessage)
     {
         bot.sendMessage(user, outputMessage);
@@ -66,10 +68,10 @@ public class CDPostBot extends ListenerAdapter
                 msg(event, stripped + " - " + URLDecoder.decode(googleMsgResults.getResponseData().getResults().get(0).getUrl(), "UTF-8"));
             }
         }
-        if(message.startsWith(".cdsearch"))
+        if (message.startsWith(".cdsearch"))
         {
             String tmp = message.substring(10);
-            
+
             googleMsgResults = google("site:chiefdelphi.com " + tmp);
             if (googleMsgResults.getResponseData().getResults().size() != 0)
             {
@@ -77,56 +79,57 @@ public class CDPostBot extends ListenerAdapter
                 msg(event, stripped + " - " + URLDecoder.decode(googleMsgResults.getResponseData().getResults().get(0).getUrl(), "UTF-8"));
             }
         }
-        if(message.startsWith(".tbaeventdata"))
+        if (message.startsWith(".tbaeventdata"))
         {
             String eventKey = message.substring(14);
-            
+
             System.out.println("getting data for " + eventKey);
-            
-            for(JsonElement element: eventList)
+
+            for (JsonElement element : eventList)
             {
                 JsonObject object = element.getAsJsonObject();
-                if(object.get("key").getAsString().equalsIgnoreCase(eventKey))
+                if (object.get("key").getAsString().equalsIgnoreCase(eventKey))
                 {
                     msg(event, "name: " + object.get("name").getAsString());
                     msg(event, "website: " + object.get("website").getAsString());
                     msg(event, "official?: " + object.get("official").getAsString());
-                    msg(event, "district: " + object.get("district").getAsString());
+                    msg(event, "district: " + object.get("event_district").getAsString());
+                    msg(event, "location: " + object.get("location").getAsString());
                 }
             }
         }
-        if(message.startsWith(".tbaeventpage"))
+        if (message.startsWith(".tbaeventpage"))
         {
             String eventKey = message.substring(14);
-            
+
             System.out.println("getting page for " + eventKey);
-            
-            for(JsonElement element: eventList)
+
+            for (JsonElement element : eventList)
             {
                 JsonObject object = element.getAsJsonObject();
-                if(object.get("key").getAsString().equalsIgnoreCase(eventKey))
+                if (object.get("key").getAsString().equalsIgnoreCase(eventKey))
                 {
                     msg(event, "http://www.thebluealliance.com/event/" + object.get("key").getAsString());
                 }
             }
         }
-        if(message.startsWith(".tbalastmatch"))
+        if (message.startsWith(".tbalastmatch"))
         {
             String eventKey = message.substring(14);
-            
+
             JsonArray array = gson.fromJson(getEventMatchesString(eventKey), JsonArray.class);
             JsonObject object = array.get(array.size() - 1).getAsJsonObject();
             int lastTime = 0;
-            for(JsonElement element: array)
+            for (JsonElement element : array)
             {
-                if(element.getAsJsonObject().get("time").getAsInt() > lastTime)
+                if (element.getAsJsonObject().get("time").getAsInt() > lastTime)
                 {
                     lastTime = element.getAsJsonObject().get("time").getAsInt();
                     object = element.getAsJsonObject();
                 }
             }
             JsonObject alliances = object.get("alliances").getAsJsonObject();
-            
+
             msg(event, "match: " + object.get("comp_level").getAsString() + object.get("match_number").getAsString());
             msg(event, "time: " + object.get("time_string").getAsString());
             msg(event, "blue: " + alliances.get("blue").getAsJsonObject().get("score").getAsString());
@@ -134,17 +137,17 @@ public class CDPostBot extends ListenerAdapter
             msg(event, "red: " + alliances.get("red").getAsJsonObject().get("score").getAsString());
             msg(event, "teams: " + alliances.get("red").getAsJsonObject().get("teams").getAsJsonArray().get(0).getAsString() + ", " + alliances.get("red").getAsJsonObject().get("teams").getAsJsonArray().get(1).getAsString() + ", " + alliances.get("red").getAsJsonObject().get("teams").getAsJsonArray().get(2).getAsString());
         }
-        if(message.startsWith(".tbagetkey"))
+        if (message.startsWith(".tbagetkey"))
         {
             String tmp = message.substring(11);
             System.out.println(tmp);
-            if(!(tmp.equalsIgnoreCase("regional")))
+            if (!(tmp.equalsIgnoreCase("regional")))
             {
-                for(JsonElement element: eventList)
+                for (JsonElement element : eventList)
                 {
                     JsonObject object = element.getAsJsonObject();
-                    
-                    if(object.get("name").getAsString().toLowerCase().contains(tmp.toLowerCase()))
+
+                    if (object.get("name").getAsString().toLowerCase().contains(tmp.toLowerCase()))
                     {
                         System.out.println(object.get("key").getAsString());
                         msg(event, object.get("key").getAsString());
@@ -153,18 +156,18 @@ public class CDPostBot extends ListenerAdapter
                 }
             }
         }
-        if(message.toLowerCase().startsWith(".tbagetmatch"))
+        if (message.toLowerCase().startsWith(".tbagetmatch"))
         {
             String[] tmp = message.toLowerCase().split("\\s+");
-            
+
             JsonArray array = gson.fromJson(getEventMatchesString(tmp[1]), JsonArray.class);
-            
-            for(JsonElement element: array)
+
+            for (JsonElement element : array)
             {
                 JsonObject object = element.getAsJsonObject();
                 String compLevelMatchNumber = object.get("comp_level").getAsString() + object.get("match_number").getAsString();
-                
-                if(compLevelMatchNumber.equalsIgnoreCase(tmp[2]))
+
+                if (compLevelMatchNumber.equalsIgnoreCase(tmp[2]))
                 {
                     JsonObject alliances = object.get("alliances").getAsJsonObject();
                     msg(event, "match: " + object.get("comp_level").getAsString() + object.get("match_number").getAsString());
@@ -176,7 +179,7 @@ public class CDPostBot extends ListenerAdapter
                 }
             }
         }
-        if(message.equalsIgnoreCase(".help"))
+        if (message.equalsIgnoreCase(".help"))
         {
             privmsg(event.getUser(), "CDBot Help:");
             privmsg(event.getUser(), ".help | PM's this help to the person who typed .help");
@@ -191,9 +194,18 @@ public class CDPostBot extends ListenerAdapter
             privmsg(event.getUser(), ".tbagetmatch <event key> <match number> | outputs the match data for the specified match for the event specified by the event key");
             privmsg(event.getUser(), "Match numbers are in the format <competition level letter><match number> | valid event levels are qm, ef, qf, sf, and f | final 1 would be f1");
         }
+        if (message.startsWith(".addEvent"))
+        {
+            String[] tmp = message.toLowerCase().split("\\s+");
+
+            addEvent(tmp[1]);
+        }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
+        Gson gson = new Gson();
+
         bot.getListenerManager().addListener(new CDPostBot());
         bot.setName("CDBot");
         bot.connect("irc.freenode.net");
@@ -205,7 +217,7 @@ public class CDPostBot extends ListenerAdapter
         getEventListString();
         getTBAEventList();
         System.out.println(eventList.get(0).getAsJsonObject().get("key"));
-        
+
         bot.sendMessage("##CDBot", previousFeed.getMessages().get(0).getTitle() + " | " + previousFeed.getMessages().get(0).getLink());
         bot.sendMessage("##FRC", previousFeed.getMessages().get(0).getTitle() + " | " + previousFeed.getMessages().get(0).getLink());
         Feed feed;
@@ -219,35 +231,74 @@ public class CDPostBot extends ListenerAdapter
                 bot.sendMessage("##CDBot", feed.getMessages().get(0).getTitle() + " | " + feed.getMessages().get(0).getLink());
                 bot.sendMessage("##FRC", feed.getMessages().get(0).getTitle() + " | " + feed.getMessages().get(0).getLink());
             }
+            
+            if (!addedEvents.isEmpty())
+            {
+                for (String eventKey : addedEvents)
+                {
+                    JsonArray array = gson.fromJson(getEventMatchesString(eventKey), JsonArray.class);
+                    JsonObject object = array.get(array.size() - 1).getAsJsonObject();
+                    int lastTime = eventLastTimes.get(eventKey);
+                    for (JsonElement element : array)
+                    {
+                        if (element.getAsJsonObject().get("time").getAsInt() > lastTime)
+                        {
+                            lastTime = element.getAsJsonObject().get("time").getAsInt();
+                            object = element.getAsJsonObject();
+
+                            JsonObject alliances = object.get("alliances").getAsJsonObject();
+
+                            bot.sendMessage("##FRC", "match: " + object.get("comp_level").getAsString() + object.get("match_number").getAsString());
+                            bot.sendMessage("##FRC", "time: " + object.get("time_string").getAsString());
+                            bot.sendMessage("##FRC", "blue: " + alliances.get("blue").getAsJsonObject().get("score").getAsString());
+                            bot.sendMessage("##FRC", "teams: " + alliances.get("blue").getAsJsonObject().get("teams").getAsJsonArray().get(0).getAsString() + ", " + alliances.get("blue").getAsJsonObject().get("teams").getAsJsonArray().get(1).getAsString() + ", " + alliances.get("blue").getAsJsonObject().get("teams").getAsJsonArray().get(2).getAsString());
+                            bot.sendMessage("##FRC", "red: " + alliances.get("red").getAsJsonObject().get("score").getAsString());
+                            bot.sendMessage("##FRC", "teams: " + alliances.get("red").getAsJsonObject().get("teams").getAsJsonArray().get(0).getAsString() + ", " + alliances.get("red").getAsJsonObject().get("teams").getAsJsonArray().get(1).getAsString() + ", " + alliances.get("red").getAsJsonObject().get("teams").getAsJsonArray().get(2).getAsString());
+                            
+                            eventLastTimes.put(eventKey, lastTime);
+                            
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 
-    public static String ping(String url) {
-        if (url.startsWith("http://")) {
-        } else if (url.startsWith("https://")) {
+    public static String ping(String url)
+    {
+        if (url.startsWith("http://"))
+        {
+        } else if (url.startsWith("https://"))
+        {
             url = url.replaceFirst("https", "http"); // Otherwise an exception may be thrown on invalid SSL certificates.
-        } else {
+        } else
+        {
             url = "http://" + url;
         }
 
-        try {
+        try
+        {
             HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
             connection.setConnectTimeout(500);
             connection.setReadTimeout(500);
             connection.setRequestMethod("HEAD");
             int responseCode = connection.getResponseCode();
 
-            if ((200 <= responseCode && responseCode <= 399) || (responseCode == 403)) {
+            if ((200 <= responseCode && responseCode <= 399) || (responseCode == 403))
+            {
                 return "successful ping with code " + responseCode;
             }
             return "unsuccessful ping with code " + responseCode;
 
-        } catch (IOException exception) {
+        } catch (IOException exception)
+        {
             return "unsuccessful ping with IOException";
         }
     }
 
-    static GoogleResults google(String query) throws IOException {
+    static GoogleResults google(String query) throws IOException
+    {
 
         String address = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
         String charset = "UTF-8";
@@ -272,75 +323,122 @@ public class CDPostBot extends ListenerAdapter
     {
         eventListString = HTTP.GET("http://www.thebluealliance.com/api/v2/events/2014");
     }
-    
+
     static void getTBAEventList()
     {
         Gson gson = new Gson();
-        
+
         eventList = gson.fromJson(eventListString, JsonArray.class);
     }
-    
+
     static String getEventMatchesString(String key)
     {
-        return HTTP.GET("http://www.thebluealliance.com/api/v2/event/"+ key + "/matches");
+        return HTTP.GET("http://www.thebluealliance.com/api/v2/event/" + key + "/matches");
+    }
+
+    static boolean addEvent(String eventKey)
+    {
+        boolean isAlreadyAdded = false;
+        for (String key : addedEvents)
+        {
+            if (key.equals(eventKey))
+            {
+                isAlreadyAdded = true;
+            }
+        }
+        if (!isAlreadyAdded)
+        {
+            Gson gson = new Gson();
+
+            addedEvents.add(eventKey);
+            eventLastTimes.put(eventKey, 0);
+
+            JsonArray array = gson.fromJson(getEventMatchesString(eventKey), JsonArray.class);
+            int lastTime = eventLastTimes.get(eventKey);
+            for (JsonElement element : array)
+            {
+                if (element.getAsJsonObject().get("time").getAsInt() > lastTime)
+                {
+                    lastTime = element.getAsJsonObject().get("time").getAsInt();
+                }
+            }
+
+            eventLastTimes.put(eventKey, lastTime);
+        }
+
+        return !isAlreadyAdded;
     }
 }
 
-class GoogleResults {
+class GoogleResults
+{
 
     private ResponseData responseData;
 
-    public ResponseData getResponseData() {
+    public ResponseData getResponseData()
+    {
         return responseData;
     }
 
-    public void setResponseData(ResponseData responseData) {
+    public void setResponseData(ResponseData responseData)
+    {
         this.responseData = responseData;
     }
 
-    public String toString() {
+    public String toString()
+    {
         return "ResponseData[" + responseData + "]";
     }
 
-    static class ResponseData {
+    static class ResponseData
+    {
 
         private List<Result> results;
 
-        public List<Result> getResults() {
+        public List<Result> getResults()
+        {
             return results;
         }
 
-        public void setResults(List<Result> results) {
+        public void setResults(List<Result> results)
+        {
             this.results = results;
         }
 
-        public String toString() {
+        public String toString()
+        {
             return "Results[" + results + "]";
         }
     }
 
-    static class Result {
+    static class Result
+    {
 
         private String url;
         private String title;
 
-        public String getUrl() {
+        public String getUrl()
+        {
             return url;
         }
 
-        public String getTitle() {
+        public String getTitle()
+        {
             return title;
         }
 
-        public void setUrl(String url) {
+        public void setUrl(String url)
+        {
             this.url = url;
         }
 
-        public void setTitle(String title) {
+        public void setTitle(String title)
+        {
             this.title = title;
         }
 
-        public String toString() {
+        public String toString()
+        {
             return "Result[url:" + url + ",title:" + title + "]";
         }
     }
